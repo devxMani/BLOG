d0cb1bdd5949438a3126aa28fbc0b73214e3e35e
MANI <chandramanigupta2003@gmail.com>
2024-09-28 23:00:49 +0530

    list

--- components/BlogList.jsx
+++ components/BlogList.jsx
@@ -1,18 +1,33 @@
 import { blog_data } from "@/Assets/assets";
 import BlogItem from "./BlogItem";
+import { useState } from "react";
 
 export default function BlogList() {
+  const [menu, setMenu] = useState("All");
+
   return (
     <div>
-      <div className="flex-justify-center gap-6 my-10">
-        <button className="bg-black text-white py-1 px-4 rounded-sm">
+      {/* Menu Buttons */}
+      <div className="flex justify-center gap-6 my-10">
+        <button
+          onClick={() => setMenu("All")}
+          className="bg-black text-white py-1 px-4 rounded-sm"
+        >
           All
         </button>
-        <button className="">Technology</button>
-        <button className="">Startup</button>
-        <button className="">Lifestyle</button>
+        <button onClick={() => setMenu("Technology")} className="">
+          Technology
+        </button>
+        <button onClick={() => setMenu("Startup")} className="">
+          Startup
+        </button>
+        <button onClick={() => setMenu("Lifestyle")} className="">
+          Lifestyle
+        </button>
       </div>
-      <div className="flex- flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24">
+
+      {/* Blog List - Modified to display horizontally */}
+      <div className="flex flex-wrap justify-around gap-4 mb-16 xl:mx-24">
         {blog_data.map((item, index) => {
           return (
             <BlogItem
--- components/Header.jsx
+++ components/Header.jsx
@@ -12,9 +12,7 @@ export default function Header() {
           className="w-[130px] sm:w-auto"
         />
         <button className="flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-solid border-black shadow-[-7px_7px_0px_#000000]">
-          Get started <Image src={assets.arrow} alt="" className="w-4" />
-          {}
-        </button>
+          Get started <Image src={assets.arrow} alt=""/></button>
       </div>
 
       <div className="text-center my-8">
@@ -26,10 +24,9 @@ export default function Header() {
         </p>
         <form className="flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-black shadow-[-7px_7px_0px_#000000]" action="">
           <input
-            type="text"
+            type="email"
             placeholder="Enter your email"
-            className="w-full py-2 px-4 outline-none"
-          />
+            className="pl-4 outline-none"/>
           <button type="submit" className="border-l border-black py-4 px-4 sm:px-8 active:bg-gray-600 active:text-white">
             Subscribe
           </button>
