7e86549625d9c180dd2a0376762acfa45413caaa
MANI <chandramanigupta2003@gmail.com>
2024-09-29 01:55:27 +0530

    resolved

--- app/blogs/[id]/page.jsx
+++ app/blogs/[id]/page.jsx
@@ -1,34 +1,43 @@
 'use client'
 import React, { useEffect, useState } from 'react'  
+import Image from 'next/image'; // Make sure you import Image from 'next/image'
 
-const { blog_data } = require("@/Assets/assets");
+const { blog_data, assets } = require("@/Assets/assets");
 
-const page = ({params}) => {
-    const [data,setData] = useState(null);
+const Page = ({ params }) => {
+    const [data, setData] = useState(null);
 
-        const fetchBlogData = ()=>{
-for (let i = 0; i < blog_data.length; i++) {
-    {
-        if(Number(params.id)===blog_data[i].id) {
-            setData(blog_data[i]);
-            console.log(blog_data[i]);
-            break;
+    const fetchBlogData = () => {
+        for (let i = 0; i < blog_data.length; i++) {
+            if (Number(params.id) === blog_data[i].id) {
+                setData(blog_data[i]);
+                console.log(blog_data[i]);
+                break;
+            }
         }
     }
-        }
+
     useEffect(() => {
         fetchBlogData();
-    }, [])
+    }, []);
 
-        
-  return (
-    <div className='bg-gray-200 py-5 px-5 md:px-12 lg:px-28'>
-    <div className='flex flex-justify-between items.center'>
-    <button className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-black shadow-[-7px_7px_0px_#000000]'>Get started</button>
-     </div>
-     </div>
-     
-  )
+    return (data ? (
+        <>
+            <div className='bg-gray-200 py-5 px-5 md:px-12 lg:px-28'>
+                <div className='flex flex-justify-between items.center'>
+                    <Image src={assets.logo} width={180} alt='' className='w-[130px] sm:w-auto' />
+                    <button className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-black shadow-[-7px_7px_0px_#000000]'>
+                        Get started 
+                        <Image src={assets.arrow} alt='' />
+                    </button>
+                </div>
+                <div className='text-center my-24'>
+                    <h1 className='text-2xl sm:text-5xl font-semibold max-w-[700px] mx-auto'>{data.title}</h1>
+                    <Image src={data.authorImg} width={60} height={60} alt='' />
+                </div>
+            </div>
+        </>
+    ) : <></>);
 }
 
-export default page
+export default Page;
