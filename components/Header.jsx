ccd6d91eadfa56a41c5b3c511ab80fe9b8289e1e
MANI <chandramanigupta2003@gmail.com>
2024-09-28 21:43:23 +0530

    lorem

--- components/Header.jsx
+++ components/Header.jsx
@@ -9,7 +9,14 @@ export default function Header() {
           src={assets.logo}
           width={180}
           alt=""
-          className="w-[130px] sm:w-auto"/>
+          className="w-[130px] sm:w-auto"
+        />
+        <button className="flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-solid border-black shadow-[-7px_7px_0px_#000000]">Get started <Image src={assets.arrow} alt="" className="w-4" />{}</button>
+      </div>
+
+      <div className="text-center my-8">
+        <h1 className="text-3xl sm:text-5xl font-medium">Latest Blogs</h1>
+      <p className="mt-10 max-w-[740px] m-auto text-xs sm:text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
       </div>
     </div>
   );
