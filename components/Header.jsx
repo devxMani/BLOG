e55c3cb6671f5caebfa1ee03e4a9daabc3e384f8
MANI <chandramanigupta2003@gmail.com>
2024-09-28 21:51:10 +0530

    ..

--- components/Header.jsx
+++ components/Header.jsx
@@ -11,12 +11,29 @@ export default function Header() {
           alt=""
           className="w-[130px] sm:w-auto"
         />
-        <button className="flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-solid border-black shadow-[-7px_7px_0px_#000000]">Get started <Image src={assets.arrow} alt="" className="w-4" />{}</button>
+        <button className="flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-solid border-black shadow-[-7px_7px_0px_#000000]">
+          Get started <Image src={assets.arrow} alt="" className="w-4" />
+          {}
+        </button>
       </div>
 
       <div className="text-center my-8">
         <h1 className="text-3xl sm:text-5xl font-medium">Latest Blogs</h1>
-      <p className="mt-10 max-w-[740px] m-auto text-xs sm:text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <p className="mt-10 max-w-[740px] m-auto text-xs sm:text-base">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit
+          amet consectetur adipisicing elit.
+        </p>
+        <form className="flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-black shadow-[-7px_7px_0px_#000000]" action="">
+          <input
+            type="text"
+            placeholder="Enter your email"
+            className="w-full py-2 px-4 outline-none"
+          />
+          <button className="border-1 border-black py-4 px-4 sm:px-8 active:bg-gray-600 active:text-white">
+            Subscribe
+          </button>
+        </form>
       </div>
     </div>
   );
