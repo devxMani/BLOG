b6c6a0a9c0e81b6c78930670f9c7fd9c45b63b89
MANI <chandramanigupta2003@gmail.com>
2024-09-29 15:12:26 +0530

    commits

--- components/AdminComponents/Sidebar.jsx
+++ components/AdminComponents/Sidebar.jsx
@@ -6,6 +6,12 @@ const Sidebar = () => {
       <div className="px-2 sm:pl-14 py-3 border border-black">
         <Image src={assets.logo} alt="" width={120} />
       </div>
+
+      <div className="w-28 sm:w-80 h-[100vh] relative py-12 border border-black">
+        <div className="flex items-center border border-black gap-3 font-medium px-3 py2 bg-white shadow -[-5px_5px_0px_#000000]">
+          <Image src={assets.add_icon} alt="" width={28} /> Add
+        </div>
+      </div>
     </div>
   );
 };
